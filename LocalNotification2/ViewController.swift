7e1e9afd7732c16c3a4dbb717a06185e2d4e67c2
MilhamAt <M477ashiddiq@gmail.com>
2020-10-11 23:15:00 +0700

    Initial Commit

---
+++ LocalNotification2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LocalNotification2
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 11/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
+class TableViewController: UITableViewController {
+    
+    let list = ["tap me",
+                "tap me"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
