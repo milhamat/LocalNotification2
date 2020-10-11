7bb1aafa83c5b0e1b0944502c494696f02f12f25
MilhamAt <M477ashiddiq@gmail.com>
2020-10-11 23:52:52 +0700

    blablabla

--- LocalNotification2/ViewController.swift
+++ LocalNotification2/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -26,8 +27,34 @@ class TableViewController: UITableViewController {
                 "tap me"
     ]
     
+    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let listType = list[indexPath.row]
+        
+        let alert = UIAlertController(title: "", message: "after 5 secon notification will apear", preferredStyle: .alert)
+        
+        let okAct = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.appDelegate?.scheduleNotification(notificationType: listType)
+        }
+        alert.addAction(okAct)
+        present(alert, animated: true, completion: nil)
     }
 }
 
